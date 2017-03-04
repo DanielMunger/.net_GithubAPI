b94ad6c792da30394cdad3473328d1c8f94d55d9
Daniel Munger <mungerda@gmail.com>
2017-03-03 16:46:12 -0800

    add restriction for more than 3 starred repos. remove api keys.

--- src/GithubApi/Models/GhApi.cs
+++ src/GithubApi/Models/GhApi.cs
@@ -10,10 +10,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using static GithubApi.Models.GithubObject;
 
+
 namespace GithubApi.Models
 {
     public class GhApi
     {
+        public object EnviornmentVariable { get; private set; }
+
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
         {
             var tcs = new TaskCompletionSource<IRestResponse>();
@@ -26,7 +29,7 @@ namespace GithubApi.Models
         public RootObject[] CallGitHubAPI()
         {
             var client = new RestClient("https://api.github.com/");        
-            var request = new RestRequest("users/DanielMunger/starred?client_id=0bc0fefe2f144068a8c9&client_secret=b722a7949a1b214c2280a5c7fab51f3bc6b76021");
+            var request = new RestRequest("users/DanielMunger/starred?client_id="+ EnvironmentVariables.ClientId+"&client_secret="+ EnvironmentVariables.ClientSecret+"");
             request.AddHeader("User-Agent", "DanielMunger");
             var response = new RestResponse();
             Task.Run(async () =>
@@ -43,6 +46,4 @@ namespace GithubApi.Models
 }
 
                                                                                                                                                                           
-//0bc0fefe2f144068a8c9 clientid
 
-//b722a7949a1b214c2280a5c7fab51f3bc6b76021 client secret
